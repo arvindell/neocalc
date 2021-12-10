1bf40a5e53ba8df71140281ad35e991fb9df7fc9
Alex Vilchis <alexrv4299@gmail.com>
2021-12-10 15:35:16 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Button from "./components/Button.svelte";
+
+  let input = "";
+</script>
+
+<main
+  class="fixed transition h-screen w-screen flex flex-col justify-center items-center bg-stone-300"
+>
+  <section
+    class="bg-gray-200 shadow-lg p-5 inline-flex flex-col gap-5 rounded-3xl border-2 border-gray-100 max-w-sm"
+  >
+    <input
+      class="rounded-2xl bg-gray-800 border-gray-900 border text-right text-white p-4 w-full text-4xl"
+      type="text"
+      name="input"
+      id="input"
+      value={input}
+    />
+    <div class="grid grid-cols-3 grid-rows-3 gap-4 max-w-md">
+      {#each [7, 8, 9, 4, 5, 6, 1, 2, 3] as num}
+        <Button number={num} on:press={(e) => (input += e.detail.text)} />
+      {/each}
+      <Button number={"X"} />
+      <Button number={"-"} />
+      <Button number={"+"} />
+      <Button number={"."} />
+    </div>
+  </section>
+</main>
---
+++ src/components/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let number;
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function onClick() {
+    dispatch("press", {
+      text: number,
+    });
+  }
+</script>
+
+<button
+  class="transition aspect-square p-1 from-slate-400 to-slate-300 border-2 border-slate-400 bg-gradient-to-br rounded-full text-slate-900 text-4xl active:from-slate-500 active:to-slate-400 active:border-slate-500 shadow-sm"
+  on:click={onClick}
+>
+  {number}
+</button>
